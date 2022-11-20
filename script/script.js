24a7687ac26e499f5f40ef1005c359a35c73f3e0
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2022-11-19 23:18:18 -0300

    ajustei e adicionei alguns botões no js

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
 var BotaoNaoResponder = document.querySelector('#nao-responder')
 var BotaoResponder = document.querySelector('#responder')
 var BotaoContinuar = document.querySelector('#responder-continuar')
+var BotaoRecarregar = document.querySelector('#responder-recarregar')
 
 BotaoNaoResponder.addEventListener('click', NaoResponder)
 BotaoResponder.addEventListener('click', Responder)
 BotaoContinuar.addEventListener('click', Continuar)
+BotaoRecarregar.addEventListener('click', Recarregar)
 
 function NaoResponder() {
     var espacoDeInteracao = document.querySelector('.centro')
@@ -19,13 +21,20 @@ function NaoResponder() {
 
 function Responder(){
     var nomeDoSujeito = document.querySelector('#nome-do-sujeito')
-    var paragrafo = document.querySelector('#resposta')
+    var resposta = document.querySelector('#resposta')
+    var pergunta = document.querySelector('#pergunta')
     var titulo = document.querySelector('#titulo')
 
     if (nomeDoSujeito.value == '') {
-        paragrafo.style.display = 'block'
-        paragrafo.innerHTML = 'Por favor, preencha o campo corretamente!! Recarregue para tentar outra vez'
-        paragrafo.style.color ='#ff6347'
+        resposta.style.display = 'block'
+        resposta.innerHTML = 'Por favor, preencha o campo corretamente!! <b>Recarregue para tentar outra vez</b>'
+        resposta.style.color ='#ff6347'
+        BotaoRecarregar.style.display = 'block'
+        BotaoRecarregar.style.backgroundColor = '#4682b4'
+        BotaoNaoResponder.value = 'Continuo não querendo responder >:('
+        pergunta.style.display = 'none'
+        nomeDoSujeito.style.display = 'none'
+        BotaoResponder.style.display = 'none'
 
     } else {
         var espacoDeInteracao = document.querySelector('.centro')
@@ -33,9 +42,11 @@ function Responder(){
         
         var secao = document.querySelector('#secao')
 
-        paragrafo.style.display = 'block'
+        resposta.style.display = 'block'
         nomeResposta.innerHTML = nomeDoSujeito.value // input
         nomeResposta.style.textTransform = 'capitalize'
+        pergunta.style.display = 'none'
+        nomeDoSujeito.style.display = 'none'
         BotaoResponder.style.display = 'none'
         BotaoNaoResponder.style.display = 'none'
         BotaoContinuar.style.display = 'block'
@@ -56,3 +67,5 @@ function Continuar() {
     espacoDeInteracao.style.display = 'none'
     secao.style.display = 'block'
 }
+
+function Recarregar() {document.location.reload(true)} // recarregar página
